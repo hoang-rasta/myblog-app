304eee90344e3bc96749556188e2fae0f4e42858
Hoàng Anh Phạm <hoanganhpham.rasta@gmail.com>
2025-06-18 01:24:16 +0700

    Initial myblog database

---
+++ src/myblog.data/MyBlogContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myblog.core;
+
+namespace myblog.data
+{
+    public class MyBlogContextFactory : IDesignTimeDbContextFactory<MyBlogContext>
+    {
+        public MyBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<MyBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new MyBlogContext(builder.Options);
+        }
+    }
+}
